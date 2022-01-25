4313e42cb3ac416828a37512cb50d073aea64604
zakaraia-lagouader <zakaria.lagouader@gmail.com>
2022-01-25 21:57:24 +0100

    added project images

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # My Portfolio 
 
 visit website : https://zakaria-lagouader.github.io/portfolio/
+
+## Built with
+
+* HTML
+* Sass
+* Javascript (anime.js for animations)
--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ modeBtn.addEventListener("click", () => {
 })
 
 function updateIcon() {
-    modeBtnIcon.src = `./img/${currentMode === "light" ? "moon" : "sun"}.png`;
+    modeBtnIcon.src = `./img/${currentMode === "light" ? "moon" : "sun"}.webp`;
 }
 
 function updateMode(mode) {
@@ -34,74 +34,4 @@ function updateMode(mode) {
     updateIcon();
 }
 
-if (window.innerWidth >= 992) {
-    // circle animation
-    anime({
-       targets: '#circle',
-       scale: [0, 1],
-       duration: 1000,
-       delay: 200,
-   }).finished.then(() => {
-       anime({
-           targets: '#circle',
-           translateX: [-10, 10, -10, 10],
-           translateY: [-10, 10, 10, -10],
-           easing: 'easeInOutSine',
-           duration: 10000,
-           loop: true,
-           direction: 'alternate',
-       });
-   })
-   // rect animation
-   anime({
-       targets: '#rect',
-       scale: [0, 1],
-       duration: 1000,
-       
-   }).finished.then(() => {
-       anime({
-           targets: '#rect',
-           translateX: [15, -15, 15, -15],
-           translateY: [15, -15, -15, 15],
-           easing: 'easeInOutSine',
-           duration: 9000,
-           loop: true,
-           direction: 'alternate',
-           rotate: 10
-       });
-   })
-   // triangle animation
-   anime({
-       targets: '#trian',
-       scale: [0, 1],
-       duration: 1000,
-       delay: 400,
-   }).finished.then(() => {
-       anime({
-           targets: '#trian',
-           translateX: [-5, 5, -5, 5],
-           translateY: [-5, 5, 5, -5],
-           easing: 'easeInOutSine',
-           duration: 8000,
-           loop: true,
-           direction: 'alternate',
-           rotate: -20
-       });
-   
-   })
-   anime({
-       targets: ['.content h1', ".content p"],
-       translateX: [-1000, 0],
-       opacity: [0, 1],
-       easing: 'easeOutExpo',
-       duration: 1200,
-       delay: 1000
-   });
-   anime({
-       targets: ["header", '#showcase .btn', '.vertical_text'],
-       opacity: [0, 1],
-       easing: 'easeOutExpo',
-       duration: 1200,
-       delay: 1200
-   });
-}
+
